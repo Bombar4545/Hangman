e109ab148c345a66db942ee0ea8fac696c251129
TK526 <36128634+TK526@users.noreply.github.com>
2018-02-04 17:02:36 +0300

    The Menu
    The menu driven part of the code

--- src/com/hangman/Main.java
+++ src/com/hangman/Main.java
@@ -1,11 +1,60 @@
-
 package com.hangman;
-
+import java.util.Scanner;
+/**
+ * Hangman Game Main Method = 1, Section 2. CS 102
+ * @author Group C, Ege Hakan KARAAGAC, Ege Ozan Ozyedek, Ufuk Bombar, Ata Sonat Uzun, Taha Khurram, Muhammad Saboor.
+ * @version 4/2/2018, 1.00
+ */
 public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("giojreigjweaoigwioegoiwrngoiwr");
+        Scanner scan = new Scanner( System.in);
+        //Constants
+
+        //Variable
+        //String guess;
+        //Hangman game;
+        boolean quit;
+        int option;
+
+        //Program Code
+        Hangman game = new Hangman();
+        quit = false;
+        System.out.println("Welcome to hangman");
+        do {
+            System.out.println("1-Start\n2-Quit");
+            option = scan.nextInt();
+            if (option == 1)
+            {
+                quit = false;
+            }
+            else if (option == 2)
+            {
+                quit = true;
+            }
+            else
+            {
+                System.out.println("Input a VALID option!");
+            }
+            if (quit)
+            {
+                System.out.println("GoodBye!!");
+            }
+            else
+                {
+                //Code for the game is over here!!!!
+                if (hasLost()) {
+                    System.out.println("You Lost");
+                } else {
+                    System.out.println("Winner Winner Chicken Dinner!!!");
+                }
+            }
+        } while (!(quit));
+
+
+
+
     }
     
 }
