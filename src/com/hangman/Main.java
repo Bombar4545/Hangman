4f8f9ce58053c552645eda7ed38dacf937ef8e36
Ufuk Bombar <ufukbombar@gmail.com>
2018-02-04 17:58:36 +0300

    Update Main.java

--- src/com/hangman/Main.java
+++ src/com/hangman/Main.java
@@ -6,7 +6,14 @@ public class Main
 
     public static void main(String[] args)
     {
-    
+        String guess;
+        
+        do
+        {
+            System.out.println( "Guess the word by typing exactly letter" );
+            guess = sc.nextLine().toLowerCase();
+        }
+        while ( guess.length() != 1  );
     }
     
 }
