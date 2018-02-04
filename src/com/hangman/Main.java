250b7d85e4f0def6ffa1e86081fe9422f6ed7c2a
msaboor35 <msaboor35@gmail.com>
2018-02-04 17:48:42 +0300

    Final creating and printing Available Alphabets

--- src/com/hangman/Main.java
+++ src/com/hangman/Main.java
@@ -1,34 +1,27 @@
 
-package com.hangman;
-
 public class Main {
-
-    public static void main(String[] args)
-    {
-    	String allLetters = game.getAllLetters();
-    	String usedLetters = game.getUsedLetters();
-    	String availableLetters;
-    	String[] allLettersArray = new String[allLetters.length()];
-        String[] usedLettersArray = new String[usedLetters.length()];
-
-    	for( int i = 0; i < allLetters.length(); i++) {
-            allLettersArray[i] = allLetters.charAt(i).toString();
-        }
-
-        for( int i = 0; i < usedLetters.length(); i++) {
-            usedLettersArray[i] = usedLetters.charAt(i).toString();
-        }
-
-        availableLetters = "";
-    	for( int i = 0; i < allLettersArray.length; i++) {
-    	    for( int j = 0; j < usedLettersArray.length; j++) {
-    	        if( usedLettersArray[j].equals( allLettersArray[i] ) ) {
-    	            availableLetters += "_ ";
-                }
-                else {
-    	            availableLetters += allLettersArray[i] + " ";
-                }
-            }
-        }
-    }
+   
+   public static void main(String[] args)
+   {
+      String allLetters = game.getAllLetters();
+      String usedLetters = game.getUsedLetters();
+      String availableLetters;
+      char[] allLettersArray = new char[allLetters.length()];
+      
+      for( int i = 0; i < allLetters.length(); i++) {
+         allLettersArray[i] = allLetters.charAt(i);
+      }
+      
+      
+      availableLetters = "";
+      for( int i = 0; i < allLettersArray.length; i++) {
+         if( usedLetters.indexOf(allLettersArray[i]) >= 0) {
+            availableLetters +=  "_ ";
+         }
+         else {
+            availableLetters += allLettersArray[i] + " ";
+         }
+      }
+      System.out.println( availableLetters );
+   }
 }
