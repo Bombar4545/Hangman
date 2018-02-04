3cae34085228c398db9e8482304e36fc50481780
Ufuk Bombar <ufukbombar@gmail.com>
2018-02-04 17:38:18 +0300

    test

--- src/com/hangman/Main.java
+++ src/com/hangman/Main.java
@@ -1,11 +1,44 @@
 
 package com.hangman;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main
+{
+    
+    static String _allLetters = "abcdefg";
+    static String _usedLetters = "af";
+    
     public static void main(String[] args)
     {
-    	System.out.println( "Saboor Test". );
+        String allLetters = _allLetters;
+        String usedLetters = _usedLetters;
+        String availableLetters;
+        String[] allLettersArray = new String[allLetters.length()];
+        String[] usedLettersArray = new String[usedLetters.length()];
+        
+        for( int i = 0; i < allLetters.length(); i++) {
+            allLettersArray[i] = allLetters.charAt(i) + "";
+        }
+        
+        for( int i = 0; i < usedLetters.length(); i++) {
+            usedLettersArray[i] = usedLetters.charAt(i) + "";
+        }
+        
+        availableLetters = "";
+        for( int i = 0; i < allLettersArray.length; i++) {
+            for( int j = 0; j < usedLettersArray.length; j++) {
+                if( usedLettersArray[j].equals( allLettersArray[i] ) ) {
+                    availableLetters += "_ ";
+                }
+                else {
+                    availableLetters += allLettersArray[i] + " ";
+                }
+            }
+        }
     }
+    
+    
+    
     
 }
