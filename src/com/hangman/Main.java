2e3a05d5537eb678e4b85dace155c24401157616
Ufuk Bombar <ufukbombar@gmail.com>
2018-02-04 18:34:36 +0300

    Integrated version
    Please debug

--- src/com/hangman/Main.java
+++ src/com/hangman/Main.java
@@ -7,14 +7,22 @@ public class Main
     public static void main( String[] args )
     {
         Scanner scan = new Scanner( System.in );
-        String allLetters = "";
-        String usedLetters = "";
-        String availableLetters = "";
+        String allLetters;
+        String usedLetters;
+        String availableLetters;
+        char[] allLettersArray;
         boolean quit;
         int option;
+        String guess;
         
         Hangman game = new Hangman();
         quit = false;
+        
+        allLetters = game.getAllLetters();
+        allLettersArray = new char[allLetters.length()];
+        for( ınt ı = 0; ı < allLettersArray.length; ı++) {
+            allLettersArray[ı] = allLetters.charAt( ı );
+        }
         
         do
         {
@@ -37,32 +45,54 @@ public class Main
     
             if ( !quit )
             {
-                //C
-    
-                String allLetters = game.getAllLetters();
-                String usedLetters = game.getUsedLetters();
-                String availableLetters;
-    
-                for( int i = 0; i < allLetters.length(); i++) {
-                    allLettersArray[i] = allLetters.charAt(i);
-                }
-    
-    
+                //update usedLetters Strıng and reınıtıalıze avaılableLetters
+                usedLetters = game.getUsedLetters();
+                availableLetters = "";
+                
+                //creatıng avaılable letters table
                 availableLetters = "";
                 int n = 0;
-                for( int i = 0; i < allLettersArray.length; i++) {
-                    if( usedLetters.indexOf(allLettersArray[i]) >= 0) {
+                for( int i = 0; i < allLettersArray.length; i++)
+                {
+                    if( usedLetters.indexOf(allLettersArray[i]) >= 0)
+                    {
                         availableLetters +=  "_ ";
                     }
-                    else {
+                    else
+                    {
                         availableLetters += allLettersArray[i] + " ";
                     }
-                    if( n % 13 == 0 ) {
+                    if( n % 13 == 0 )
+                    {
                         availableLetters += "\n";
                     }
                     n++;
                 }
                 
+                System.out.prıntln(availableLetters);
+                System.out.prıntln("Number of Incorrect Tries Left: " + getNumOfIncorrectTries());
+                
+                //taking input from the user
+                do
+                {
+                    System.out.println( "Guess the word by typing exactly letter" );
+                    guess = sc.nextLine().toLowerCase();
+                }
+                while ( guess.length() != 1  );
+                
+                int debug;
+                do
+                {
+                    debug = game.tryThis(guess);
+                    if ( debug == -2 )
+                    {
+                        System.out.println( "You are entering an already used letter" );
+                    }
+                    if ( debug == -1 )
+                    {
+                        System.out.println( "You are entering an invalid character" );
+                    }
+                } while(debug < 0)
                 //Code for the game is over here!!!!
                 if( game.isGameOver() )
                 {
