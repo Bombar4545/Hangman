5dfdedbeed19a8de4fd24e6558d6090df8ec4127
TK526 <36128634+TK526@users.noreply.github.com>
2018-02-05 19:30:25 +0300

    Comments

--- src/com/hangman/Main.java
+++ src/com/hangman/Main.java
@@ -1,49 +1,64 @@
-package com.hangman;
-
 import java.util.Scanner;
 
-public class Main2
+
+public class Main
 {
 	public static void main( String[] args)
 	{
+		//declaring variables
 		Scanner scan = new Scanner( System.in );
 		String guess;
 		int tmp;
 		Hangman game;
 		
+		//initializing hangman game
 		game = new Hangman();
+		
+		//this block runs while game is running
 		do
 		{
+			//display hangman stats
 			System.out.println( "* Used Letters: " + game.getUsedLetters() );
 			System.out.println( "* Number of Incorrect Tries: " + game.getNumOfIncorrectTries() );
 			System.out.println( "* Known So Far: " + game.getKnownSoFar() );
 			System.out.println();
 			
+			//getting valid user input
 			do
 			{
+				System.out.print( "Please enter a letter: " );
 				guess = scan.nextLine();
 			}
-			while( guess.length() != 0 );
+			while( guess.length() == 0 );
 			
-			
+			//getting first character of user input, rest is ignored
 			guess = guess.toLowerCase().charAt(0) + "";
 			tmp = game.tryThis( guess );
 			
-			//check all cases
+			//check all cases and errors from tryThis method
 			if ( tmp == -1 )
-				System.out.print( "The letter is not valid" );
+			{
+				System.out.println( "The letter is not valid" );
+			}
 			else if ( tmp == -2 )
+			{
 				System.out.println( "The letter was already used" );
+			}
 			else if ( tmp > 0 )
-				System.out.print( tmp + "matchs!!!" );
-			
+			{
+				System.out.println( tmp + "matchs!!!" );
+			}
 		}
 		while ( !game.isGameOver() );
 		
+		//checking if the player has won or lost
 		if( game.hasLost() )
-			System.out.print("You Lost!");
+		{
+			System.out.println("You Lost!");
+		}
 		else
-			System.out.print("You Win!");
+		{
+			System.out.println("You Win!");
+		}
 	}
-	
 }
